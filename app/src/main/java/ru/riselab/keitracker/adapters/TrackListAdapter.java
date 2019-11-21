a2f022a7ac50cd6a7e651e984f3a48267dd62f50
Kei <dkey87@gmail.com>
2019-11-21 21:17:50 +0500

    Track list view updated

--- app/src/main/java/ru/riselab/keitracker/adapters/TrackListAdapter.java
+++ app/src/main/java/ru/riselab/keitracker/adapters/TrackListAdapter.java
@@ -1,6 +1,7 @@
 package ru.riselab.keitracker.adapters;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import ru.riselab.keitracker.R;
 import ru.riselab.keitracker.db.pojo.Track;
@@ -46,8 +50,12 @@ public class TrackListAdapter extends RecyclerView.Adapter<TrackListAdapter.Trac
     public void onBindViewHolder(@NonNull TrackViewHolder holder, int position) {
         if (mTracks != null) {
             Track current = mTracks.get(position);
-            holder.trackItemNameView.setText(current.trackUuid);
-            holder.trackItemInfoView.setText(String.format("From %s to %s", current.firstTime, current.lastTime));
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.getDefault());
+            String trackInfoString = String.format("<i>from <b>%s</b> to <b>%s</b></i>",
+                    dateFormat.format(current.firstTime),
+                    dateFormat.format(current.lastTime));
+            holder.trackItemNameView.setText(String.format("%s) %s", position + 1, current.trackUuid));
+            holder.trackItemInfoView.setText(Html.fromHtml(trackInfoString));
         } else {
             // TODO: process data not ready case
         }
