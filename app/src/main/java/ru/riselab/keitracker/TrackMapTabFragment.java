402f6074a6d5e7b71555a174223f219bad936f42
Kei <dkey87@gmail.com>
2019-11-30 00:55:16 +0500

    Show entire track on map opening

--- app/src/main/java/ru/riselab/keitracker/TrackMapTabFragment.java
+++ app/src/main/java/ru/riselab/keitracker/TrackMapTabFragment.java
@@ -2,15 +2,12 @@ package ru.riselab.keitracker;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,17 +16,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
-import ru.riselab.keitracker.db.AppDatabase;
-import ru.riselab.keitracker.db.dao.LocationDao;
 import ru.riselab.keitracker.db.model.LocationModel;
 import ru.riselab.keitracker.db.viewmodel.LocationViewModel;
 
@@ -56,48 +49,50 @@ public class TrackMapTabFragment extends Fragment implements OnMapReadyCallback 
         TrackActivity trackActivity = (TrackActivity) getActivity();
         String trackUuid = (trackActivity != null) ? trackActivity.getTrackUuid() : "";
 
-        // TODO: create repository for map and refactor
+        // TODO: create repository for map if possible
         locationViewModel.getTrackLocations(trackUuid).observe(
-                this.getViewLifecycleOwner(), new Observer<List<LocationModel>>() {
-                    @Override
-                    public void onChanged(List<LocationModel> points) {
+                this.getViewLifecycleOwner(), points -> {
+                    if (points.size() != 0 && mMap != null) {
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        PolylineOptions polylineOptions = new PolylineOptions();
                         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.getDefault());
-                        if (points.size() != 0) {
-                            if (mMap != null) {
-                                mMap.clear();
-                            }
+
+                        mMap.clear();
+
+                        for (LocationModel point : points) {
                             LatLng latLng = new LatLng(
-                                    points.get(0).getLatitude(),
-                                    points.get(0).getLongitude()
+                                    point.getLatitude(),
+                                    point.getLongitude()
                             );
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
-                            PolylineOptions polylineOptions = new PolylineOptions();
-                            for (LocationModel point : points) {
-                                latLng = new LatLng(
-                                        point.getLatitude(),
-                                        point.getLongitude()
-                                );
-                                CircleOptions circleOptions = new CircleOptions()
-                                        .center(latLng)
-                                        .radius(3)
-                                        .strokeWidth(10)
-                                        .strokeColor(ResourcesCompat.getColor(getResources(), R.color.colorAccent, null))
-                                        .fillColor(ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null))
-                                        .zIndex(11);
-                                polylineOptions.add(latLng)
-                                        .color(ResourcesCompat.getColor(getResources(), R.color.colorAccent, null))
-                                        .width(10)
-                                        .zIndex(10);
-                                mMap.addCircle(circleOptions);
-                            }
-                            mMap.addPolyline(polylineOptions);
+
+                            CircleOptions circleOptions = new CircleOptions()
+                                    .center(latLng)
+                                    .radius(3)
+                                    .strokeWidth(10)
+                                    .strokeColor(ResourcesCompat.getColor(getResources(), R.color.colorAccent, null))
+                                    .fillColor(ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null))
+                                    .zIndex(11);
+                            mMap.addCircle(circleOptions);
+
+                            builder.include(latLng);
+
+                            polylineOptions.add(latLng)
+                                    .color(ResourcesCompat.getColor(getResources(), R.color.colorAccent, null))
+                                    .width(10)
+                                    .zIndex(10);
                         }
+
+                        mMap.addPolyline(polylineOptions);
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
                     }
                 }
         );
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
 
         return rootView;
     }
