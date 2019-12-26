0ce6dd4c34a89a7bdb6fbec0b5a71aa18c713aa0
Kei <dkey87@gmail.com>
2019-12-26 11:24:19 +0500

    AboutActivity with Privacy Policy link implemented

---
+++ app/src/main/java/ru/riselab/keitracker/AboutActivity.java
@@ -0,0 +1,22 @@
+package ru.riselab.keitracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TextView privacyPolicyLink = (TextView) findViewById(R.id.aboutPrivacyPolicy);
+        privacyPolicyLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView termsAndConditionsLink = (TextView) findViewById(R.id.aboutTermsAndConditions);
+        termsAndConditionsLink.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
